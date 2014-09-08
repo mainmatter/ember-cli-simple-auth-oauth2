9f76b61daf041826e946be3198ef552731f7f7ca
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-09-08 11:01:21 +0200

    switch to new ember-cli addon structure

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
-var path = require('path');
-var fs   = require('fs');
+module.exports = {
+  name: 'ember-cli-simple-auth-oauth2',
 
-function EmberCLISimpleAuthOauth2(project) {
-  this.project = project;
-  this.name    = 'Ember CLI Simple Auth OAuth 2.0';
-}
+  included: function(app) {
+    this._super.included(app);
 
-function unwatchedTree(dir) {
-  return {
-    read:    function() { return dir; },
-    cleanup: function() { }
-  };
-}
-
-EmberCLISimpleAuthOauth2.prototype.treeFor = function included(name) {
-  var treePath = path.join('node_modules/ember-cli-simple-auth-oauth2', name + '-addon');
-
-  if (fs.existsSync(treePath)) {
-    return unwatchedTree(treePath);
+    this.app.import('bower_components/ember-simple-auth/simple-auth-devise.amd.js', {
+      exports: {
+        'simple-auth-devise/authenticators/devise': ['default'],
+        'simple-auth-devise/authorizers/devise':    ['default'],
+        'simple-auth-devise/initializer':           ['default']
+      }
+    });
   }
-};
-
-EmberCLISimpleAuthOauth2.prototype.included = function included(app) {
-  this.app = app;
-
-  this.app.import('vendor/ember-simple-auth/simple-auth-oauth2.amd.js', {
-    exports: {
-      'simple-auth-oauth2/authenticators/oauth2': ['default'],
-      'simple-auth-oauth2/authorizers/oauth2':    ['default'],
-      'simple-auth-oauth2/initializer':           ['default']
-    }
-  });
-};
-
-module.exports = EmberCLISimpleAuthOauth2;
+}
