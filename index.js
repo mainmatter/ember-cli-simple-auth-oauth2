6ee7fbb1dba2a04e846b8fba3feb3213178e3f42
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-09-29 16:43:45 +0200

    export the configuration module

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ module.exports = {
     this.app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth-oauth2.amd.js', {
       exports: {
         'simple-auth-oauth2/authenticators/oauth2': ['default'],
-        'simple-auth-oauth2/authorizers/oauth2':    ['default']
+        'simple-auth-oauth2/authorizers/oauth2':    ['default'],
+        'simple-auth-oauth2/configuration':         ['default']
       }
     });
   }
